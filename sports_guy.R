e2cdfcfe6e495ad7b631b2cbce26abfa93129779
Evan Thompson <42624025+EvanT33@users.noreply.github.com>
2019-07-03 15:35:45 -0700

    Update sports_guy.R

--- sports_guy.R
+++ sports_guy.R
@@ -1,7 +1,10 @@
-
+# This program pulls FiveThirtyEight MLB game probabilities, along
+# with Vegas odds, and then identifies all bets each day with 
+# an expected return over [3%]. Go hawks.
 
 # load packages
 library(rvest)
+library(gdata)
 
 # FiveThirtyEight MLB game predictions
 url <- 'https://projects.fivethirtyeight.com/2019-mlb-predictions/games/'
@@ -48,23 +51,126 @@ for (k in 2:nrow(data)) {
     } else {
       data$gamedate[k] <- data$gamedate[k]
     }
-  }
-  
-### library(tidyr), spread()???
+}
+
+data <- data[-which(data$type == "date"), ]
 
 
-# reformat data
-# limit to today's date
-# pull in odds
-# merge odds (probably need a key to standardize  team names)
+####################################################
+############### FiveThirtyEight Odds ###############
+####################################################
+
+
+
+# Using CSS selectors to scrape the WP section
+winp_html <- html_nodes(webpage,'.win-prob')
+
+# Converting the WP data to text
+winp <- html_text(winp_html)
+
+
+# Convert to dataframe, reformat
+winp <- as.data.frame(winp)
+winp <- winp[-1, ]
+winp <- as.data.frame(winp)
+
+# Remove bad
+bad <- which(grepl("Win prob.Chance of winning", winp$winp))
+winp <- winp[-bad, ]
+winp <- as.data.frame(winp)
+
+
+
+
+# Combined game info and win probs
+data <- cbind(data, winp)
+names(data)[1] <- "team"
+
+
+# create game_id and clean data into correct formats
+data$g_id <- ""
+for (k in 1:nrow(data)) {
+  data$g_id[k] <- ceiling(k/2)
+}
+cols <- c("gamedate", "g_id", "team", "winp")
+data <- data[ ,cols]
+data$winp <- gsub("%", "", data$winp)
+data$winp <- as.numeric(data$winp)/100
+today <- Sys.Date()
+data$gamedate <- sapply(strsplit(data$gamedate, split=', ', fixed=TRUE), function(x) (x[2]))
+data$gamedate <- as.numeric(sapply(strsplit(data$gamedate, split=' ', fixed=TRUE), function(x) (x[2])))
+today <- as.numeric(substr(today, 9, 10))
+data <- data[today == data$gamedate, ]
+data <- data[!duplicated(data$team),]
+
+
+
+# Vegas Insider MLB game predictions
+url <- 'https://www.actionnetwork.com/mlb/live-odds'
+webpage <- read_html(url)
+
+#################################################
+############### [[]] ###############
+#################################################
+
+# Using CSS selectors to scrape the Vegas section
+vegas_html <- html_nodes(webpage,'.h-h3')
+vegas_odds_html <- html_nodes(webpage,'.mainOdds__row')
+
+# Converting the Vegas data to text
+vegas <- html_text(vegas_html)
+vegas_odds <- html_text(vegas_odds_html)
+
+# Convert to dataframe, reformat
+vegas <- as.data.frame(vegas)
+vegas_odds <- as.data.frame(vegas_odds)
+vegas_odds$vegas_odds <- substr(vegas_odds$vegas_odds, 5, 8)
+vegas$vegas <- as.character(vegas$vegas)
+vegas$keep <- ""
+vegas$keep <- ifelse(nchar(vegas$vegas)>1, 1, 0)
+vegas <- vegas[which(vegas$keep == 1), ]
+vegas <- cbind(vegas$vegas, vegas_odds)
+names(vegas)[1] <- "abbr"
+vegas <- vegas[!duplicated(vegas$abbr),]
+
+
+# team mappings
+team <- c("Marlins", 	"Nationals", 	"Marlins", 	"Nationals", 	"Cubs", 	
+          "Pirates", 	"Red Sox", 	"Blue Jays", 	"Yankees", 	"Mets", 	
+          "Orioles", 	"Rays", 	"Brewers", 	"Reds", 	"Phillies", 	
+          "Braves", 	"Angels", 	"Rangers", 	"Astros", 	"Rockies", 	
+          "Tigers", 	"White Sox", 	"Indians", 	"Royals", 	"Twins", 	
+          "Athletics", 	"Giants", 	"Padres", 	"Diamondbacks", 	
+          "Dodgers", 	"Cardinals", 	"Mariners", 	"Tigers", 	"White Sox")
+abbr <- c("MIA", 	"WSH", 	"MIA", 	"WSH", 	"CHC", 	"PIT", 	"BOS", 	"TOR", 	
+          "NYY", 	"NYM", 	"BAL", 	"TB", 	"MIL", 	"CIN", 	"PHI", 	"ATL", 	
+          "LAA", 	"TEX", 	"HOU", 	"COL", 	"DET", 	"CWS", 	"CLE", 	"KC", 	
+          "MIN", 	"OAK", 	"SF", 	"SD", 	"ARI", 	"LAD", 	"STL", 	"SEA", 	
+          "DET", 	"CWS")
+team_map <- cbind(team, abbr)
+
+
+# merge team abbreviations onto FiveThiryEight data
+data <- merge(data, team_map, by = "team")
+
+# merge vegas odds onto FiveThirtyEight data
+data_master <- merge(data, vegas, by = "abbr")
+data_master$g_id <- as.numeric(data_master$g_id)
+data_master <- data_master[order(data_master$g_id),] 
+data_master <- data_master[!duplicated(data_master),]
+data_master$gamedate <- Sys.Date()
+data_master$vegas_odds <- as.numeric(data_master$vegas_odds)
+
+
+# remove unneeded datasets
+keep(data_master, sure = TRUE)
+
+
+
 # do the math
 # limit to the good bets
 # identify the bok
 # automate sports_guy
-
-
-
-
 
 
 
